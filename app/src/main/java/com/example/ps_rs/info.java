9d1ee38a360df0ffc63b7b87bb708bb9d4471854
ricky <ricky10297@gmail.com>
2021-12-09 11:55:07 -0500

    ricardo cambio2

--- app/src/main/java/com/example/ps_rs/info.java
+++ app/src/main/java/com/example/ps_rs/info.java
@@ -2,7 +2,9 @@ package com.example.ps_rs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class info extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+    }
+    public void onClick(View view ){
+        switch (view.getId()){
+            case R.id.r5:
+                Intent intent = new Intent(info.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                break;
+        }
     }
 }
